f20bdfa2245b3febca873fcc44968bfdbe593425
Youssef Ashraf <globalyoussef2@gmail.com>
2023-06-22 02:13:24 +0300

    final code

--- lib/MCAL/MUART/MUART_program.c
+++ lib/MCAL/MUART/MUART_program.c
@@ -5,31 +5,55 @@
  *      Author: Global
  */
 
-
-#include "../../LIB/LIB_STD_TYPES.h"
-#include "../../LIB/LIB_BIT_MATH.h"
-
 #include "MUART_private.h"
-//#include "MUART_interface.h"	
-#include <avr/io.h>
+#include "MUART_interface.h"	
 
-void MUART_voidInit(void)
+void UART_init(const UART_ConfigType* Config_Ptr)
 {
+	unsigned short ubrr_value = 0;
 
-	UCSRB |= (1 << RXEN);								/* Enable USART transmitter and receiver */
-	UCSRC |= (1 << URSEL)| (1 << UCSZ0) | (1 << UCSZ1);	/* Write USCRC for 8 bit data and 1 stop bit */ 
-	UBRRL = 51;											/* Load UBRRL with lower 8 bit of prescale value */
-	UBRRH = (51>>8);									/* Load UBRRH with upper 8 bit of prescale value */
+	/* U2X = 1 for double transmission speed */
+	UCSRA = (1<<U2X);
+
+	UCSRB = (1<<RXEN) | (1<<TXEN);
+	
+	SET_BIT(UCSRA,URSEL);
+	UCSRC = (UCSRC & 0xCF) | ((Config_Ptr -> en_parity & 0x03) << 4); /* Select the type of parity bit */
+	UCSRC = (UCSRC & 0xF7) | ((Config_Ptr -> en_stop   & 0x01) << 3); /* Select number of stop bits */
+	UCSRC = (UCSRC & 0xF8) | (Config_Ptr -> en_data    & 0x07);       /* Select number of data bits */
+	
+	/* Calculate the UBRR register value */
+	ubrr_value = (unsigned short)(((1000000UL / (Config_Ptr -> baudRate * 8UL))) - 1);
+
+	/* First 8 bits from the BAUD_PRESCALE inside UBRRL and last 4 bits in UBRRH*/
+	UBRRH = ubrr_value>>8;
+	UBRRL = ubrr_value;
 }
 
-void MUART_voidSendChar(u8 Copy_u8Data)
+void UART_sendByte(const unsigned char data)
 {
-	UDR = Copy_u8Data;								/* Write data to be transmitting in UDR */
-	while (!(UCSRA & (1<<UDRE)));					/* Wait until data transmit and buffer get empty */
+	/*
+	 * UDRE flag is set when the Tx buffer (UDR) is empty and ready for
+	 * transmitting a new byte so wait until this flag is set to one
+	 */
+	while(!GET_BIT(UCSRA,UDRE)){}
+
+	/*
+	 * Put the required data in the UDR register and it also clear the UDRE flag as
+	 * the UDR register is not empty now
+	 */
+	UDR = data;
+
 }
 
-u8 MUART_u8ReceiveChar(void)
+unsigned char UART_recieveByte(void)
 {
-	while (!(UCSRA & (1 << RXC)));				/* Wait until new data receive */
-	return UDR;									/* Get and return received data */ 
+	/* RXC flag is set when the UART receive data so wait until this flag is set to one */
+	while(!GET_BIT(UCSRA,RXC)){}
+
+	/*
+	 * Read the received data from the Rx buffer (UDR)
+	 * The RXC flag will be cleared after read the data
+	 */
+    return UDR;		
 }
